daf472e3cb34432d60a878682fc7cc0308570508
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2022-06-07 11:50:37 -0300

    Refactoring my old code
    Sorry for mistakes, im learning every day

--- main.c
+++ main.c
@@ -1,96 +1,92 @@
 #include <stdio.h>
 #include <string.h>
 #define MAX 20
-typedef struct {
-	char marca[16];
-	int ano;
-	char cor[11];
-	float preco;
-}carro;
+typedef struct{
+	char brand[16];
+	int year;
+	char color[11];
+	float price;
+} carro;
 
 int main(){
 	int i;
-	carro vetcarro[20];
-	float avaliar;
-	int contador;
-	char carro_avaliar_marca[16];
-	int carro_avaliar_ano;
-	char carro_avaliar_cor[11];
-	for(i=0;i<MAX;i++){
-		printf("CAR %d\n",i+1);
+	carro car_array[20];
+	float toEvaluate;
+	int count;
+	char car_evaluate_brand[16];
+	int car_evaluate_year;
+	char car_evaluate_color[11];
+	for (i = 0; i < MAX; i++){
+		printf("CAR %d\n", i + 1);
 		printf("brand:\n");
-		scanf("%s",vetcarro[i].marca);
+		scanf("%s", car_array[i].brand);
 		printf("year:\n");
-		scanf("%d",&vetcarro[i].ano);
+		scanf("%d", &car_array[i].year);
 		printf("color:\n");
-		scanf("%s",vetcarro[i].cor);
+		scanf("%s", car_array[i].color);
 		printf("price\n");
-		scanf("%f",&vetcarro[i].preco);
+		scanf("%f", &car_array[i].price);
 	}
 	printf("set a price:\n");
-	scanf("%f",&avaliar);
+	scanf("%f", &toEvaluate);
 	printf("cars with the same or lower price:\n");
-	
-	for(i=0,contador=0;i<MAX;i++){
-		if(vetcarro[i].preco <= avaliar ){
-			contador = contador + 1;
-			printf("brand: %s\n",vetcarro[i].marca);
-			printf("color: %s\n",vetcarro[i].cor);
-			printf("year: %d\n",vetcarro[i].ano);		
-						
-		}else {
-			continue;
-		}
-	}
-	if(contador == 0){
-		printf("do not have any car with the same or lower price");
-	}else{
-		printf("number of assembled cars = %d\n", contador);
-	}
-	
 
-	
-	printf("Set a car brand\n");
-	scanf("%s",carro_avaliar_marca);
-	
-	for(i=0,contador=0;i<MAX;i++){
-		
-		if(strcmp(carro_avaliar_marca,vetcarro[i].marca)==0){
-			contador = contador + 1;
-			printf("price: %.2f\n",vetcarro[i].preco);
-			printf("year: %d\n",vetcarro[i].ano);
-			printf("color: %s\n",vetcarro[i].cor);
-	
-		}else{
+	for (i = 0, count = 0; i < MAX; i++){
+		if (car_array[i].price <= toEvaluate){
+			count = count + 1;
+			printf("brand: %s\n", car_array[i].brand);
+			printf("color: %s\n", car_array[i].color);
+			printf("year: %d\n", car_array[i].year);
+		}
+		else{
 			continue;
 		}
-		
 	}
-	if (contador == 0){
-		printf("dont have any cars with this brand\n");
-	}else{
-		printf("number of assembled cars = %d\n", contador);
+	if (count == 0){
+		printf("do not have any car with the same or lower price");
 	}
-	
-	printf("Set the brand, year and color of a car:\n");
-	scanf(" %s %d %s",carro_avaliar_marca,&carro_avaliar_ano,carro_avaliar_cor);
-	printf("Cars with the same mark, year and color:\n");
-	for(i=0,contador=0;i<MAX;i++){
-		if(strcmp(carro_avaliar_marca,vetcarro[i].marca)==0 && strcmp(carro_avaliar_cor,vetcarro[i].cor)==0 && carro_avaliar_ano==vetcarro[i].ano){
-			contador = contador + 1;
-			printf("price: %.2f\n",vetcarro[i].preco);
-		}else{
+	else{
+		printf("number of assembled cars = %d\n", count);
+	}
+
+	printf("Set a car brand\n");
+	scanf("%s", car_evaluate_brand);
+
+	for (i = 0, count = 0; i < MAX; i++){
+
+		if (strcmp(car_evaluate_brand, car_array[i].brand) == 0){
+			count = count + 1;
+			printf("price: %.2f\n", car_array[i].price);
+			printf("year: %d\n", car_array[i].year);
+			printf("color: %s\n", car_array[i].color);
+		}
+		else{
 			continue;
-		}		
+		}
 	}
-	if (contador==0){
+
+	if (count == 0){
+		printf("dont have any cars with this brand\n");
+	}
+	else{
+		printf("number of assembled cars = %d\n", count);
+	}
+
+	printf("Set the brand, year and color of a car:\n");
+	scanf(" %s %d %s", car_evaluate_brand, &car_evaluate_year, car_evaluate_color);
+	printf("Cars with the same mark, year and color:\n");
+	for (i = 0, count = 0; i < MAX; i++){
+		if (strcmp(car_evaluate_brand, car_array[i].brand) == 0 && strcmp(car_evaluate_color, car_array[i].color) == 0 && car_evaluate_year == car_array[i].year){
+			count = count + 1;
+			printf("price: %.2f\n", car_array[i].price);
+		}
+		else{
+			continue;
+		}
+	}
+	if (count == 0){
 		printf("No cars with these same features were found\n");
 	}
-	
-	
-	
-	
-	
-	
+
 	return 0;
 }
